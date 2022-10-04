2d8e0c21dd2ceff992754088df4c67803ca8fdef
ulziiiii <ulziisurendavaasuren@gmail.com>
2022-10-04 23:18:31 +0300

    Initial commit

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ discr = b ** 2 - 4 * a * c
 print("Дискриминант D = %.2f" % discr)
 
 if discr > 0:
-x1 = (-b + math.sqrt(discr)) / (2 * a)
-x2 = (-b - math.sqrt(discr)) / (2 * a)
-print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+    x1 = (-b + math.sqrt(discr)) / (2 * a)
+    x2 = (-b - math.sqrt(discr)) / (2 * a)
+    print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
 elif discr == 0:
-x = -b / (2 * a)
-print("x = %.2f" % x)
+   x = -b / (2 * a)
+   print("x = %.2f" % x)
 else:
-print("Корней нет")
+    print("Корней нет")
 
