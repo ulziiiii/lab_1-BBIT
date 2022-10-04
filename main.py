8a02447e07e9d63674773379d54fa0bf3e6a0141
ulziiiii <ulziisurendavaasuren@gmail.com>
2022-10-04 19:20:03 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-sides = [3,2,4,7,5,12,11,13,15,16,14,14]
+import math
 
-sides = sorted(sides, reverse=True)
+print("Введите коэффициенты для уравнения")
+print("ax^2 + bx + c = 0:")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
 
-smax = 0
-for i in range(len(sides)):
-    for j in range(i+1,len(sides)):
-        for k in range(j+1,len(sides)):
-            a = sides[i]
-            b = sides[j]
-            c = sides[k]
-            if a+b > c and a+c > a:
-                p = (a+ b+ c) /2
-                s = (p*(p-a)*(p-b)*(p-c))**(1/2)
-                if s > smax:
-                    smax = s
+discr = b ** 2 - 4 * a * c
+print("Дискриминант D = %.2f" % discr)
 
-                    print ("максималная площадь триугольника", smax)
-
+if discr > 0:
+x1 = (-b + math.sqrt(discr)) / (2 * a)
+x2 = (-b - math.sqrt(discr)) / (2 * a)
+print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+elif discr == 0:
+x = -b / (2 * a)
+print("x = %.2f" % x)
+else:
+print("Корней нет")
 
